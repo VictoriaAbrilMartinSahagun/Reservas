70f22e5566f926ae92d6b73a05aad3eb8df1e116
VictoriaMartinSahagun <vams.00@hotmail.com>
2024-11-04 13:59:35 -0300

    eliminoCors

--- js/precioAPI.js
+++ js/precioAPI.js
@@ -1,5 +1,8 @@
 export async function getPriceData() {
-    const response = await fetch('https://script.google.com/macros/s/AKfycbyZ_i2fUxiO2WTRJl97mSpbWmZHp7beXMM46z4nRKD-0EloXIOiEILEFghbcwb0Qqun/exec');
+    const response = await fetch('https://script.google.com/macros/s/AKfycbyZ_i2fUxiO2WTRJl97mSpbWmZHp7beXMM46z4nRKD-0EloXIOiEILEFghbcwb0Qqun/exec', {
+        method: 'GET',
+        mode: 'no-cors' // Desactiva CORS
+      });
     const jsonData = await response.json();
     const priceData = jsonData.data;
     return priceData;
