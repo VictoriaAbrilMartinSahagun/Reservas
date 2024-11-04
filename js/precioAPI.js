209567f2eb7dd54abcd9f1f9f943e6abcb4e5068
VictoriaMartinSahagun <vams.00@hotmail.com>
2024-11-04 13:29:31 -0300

    CambioUrlAPI

--- js/precioAPI.js
+++ js/precioAPI.js
@@ -1,5 +1,5 @@
 export async function getPriceData() {
-    const response = await fetch('https://script.google.com/macros/s/AKfycbyVkSt3EuAZSrJJLKI5V0B6IYUjG3xfQJ9lQy67nd3hyZiDPhT_j4Sk-P6TgOMA5apW/exec');
+    const response = await fetch('https://script.google.com/macros/s/AKfycbyOF-qFKmO2hYb2tanXu4Z4MEW5NMQ7bC-7e2h8twgg8_Mt6A0fKyealg3jEgVfHolY/exec');
     const jsonData = await response.json();
     const priceData = jsonData.data;
     return priceData;
