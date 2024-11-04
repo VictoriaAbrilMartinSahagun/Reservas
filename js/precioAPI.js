1b0dd86398225269d6011e30c1161c966dbb19e3
VictoriaMartinSahagun <vams.00@hotmail.com>
2024-11-04 14:03:55 -0300

    Update precioAPI.js

--- js/precioAPI.js
+++ js/precioAPI.js
@@ -1,5 +1,14 @@
 export async function getPriceData() {
-    const response = await fetch('https://script.google.com/macros/s/AKfycbyZ_i2fUxiO2WTRJl97mSpbWmZHp7beXMM46z4nRKD-0EloXIOiEILEFghbcwb0Qqun/exec', {mode: 'no-cors'});
+    const response = await fetch('https://script.google.com/macros/s/AKfycbw2bOFSKh9Mp3xAdcvTQ8BrjnLLyEs7ik8bcJQHcEtSE7mBUufLY7QqcprhOWjedvf6/exec', 
+        {   method: 'GET',
+            mode: 'no-cors' // Desactiva CORS
+        })
+    .then(response => {
+        console.log('Solicitud enviada, pero no se puede acceder a la respuesta debido a CORS.');
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
     const jsonData = await response.json();
     const priceData = jsonData.data;
     return priceData;
