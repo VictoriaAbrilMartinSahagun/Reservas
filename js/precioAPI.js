de92dac5bcdd26ecd62aec7a2ab3beebb54ada67
VictoriaAbrilMartinSahagun <victoriamartinsah@gmail.com>
2024-11-04 13:56:32 -0300

    Update precioAPI.js

--- js/precioAPI.js
+++ js/precioAPI.js
@@ -1,5 +1,5 @@
 export async function getPriceData() {
-    const response = await fetch('https://script.google.com/macros/s/AKfycbyOF-qFKmO2hYb2tanXu4Z4MEW5NMQ7bC-7e2h8twgg8_Mt6A0fKyealg3jEgVfHolY/exec');
+    const response = await fetch('https://script.google.com/macros/s/AKfycbyZ_i2fUxiO2WTRJl97mSpbWmZHp7beXMM46z4nRKD-0EloXIOiEILEFghbcwb0Qqun/exec');
     const jsonData = await response.json();
     const priceData = jsonData.data;
     return priceData;
