19abdb2cf06e95b7a39d967e541b389872d29900
VictoriaMartinSahagun <vams.00@hotmail.com>
2024-11-04 14:00:47 -0300

    Update precioAPI.js

--- js/precioAPI.js
+++ js/precioAPI.js
@@ -1,8 +1,5 @@
 export async function getPriceData() {
-    const response = await fetch('https://script.google.com/macros/s/AKfycbyZ_i2fUxiO2WTRJl97mSpbWmZHp7beXMM46z4nRKD-0EloXIOiEILEFghbcwb0Qqun/exec', {
-        method: 'GET',
-        mode: 'no-cors' // Desactiva CORS
-      });
+    const response = await fetch('https://script.google.com/macros/s/AKfycbyZ_i2fUxiO2WTRJl97mSpbWmZHp7beXMM46z4nRKD-0EloXIOiEILEFghbcwb0Qqun/exec', {mode: 'no-cors'});
     const jsonData = await response.json();
     const priceData = jsonData.data;
     return priceData;
